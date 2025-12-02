ec71fa6174fc3ffe3942560679253048dc62a531
Rajeev Kumar <rajeevkumarrj7@gmail.com>
2025-12-02 16:08:11 +0530

    adding working cart adding and removing the items in cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,38 @@
+import { useContext } from 'react';
 import Modal from '../UI/Modal';
+import CartContext from '../../store/cart-context';
 import classes from './Cart.module.css';
 
 const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
+
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({ ...item, amount: 1 });
+  };
+
   const cartItems = (
     <ul className={classes['cart-items']}>
-      {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map((item) => (
-        <li>{item.name}</li>
+      {cartCtx.items.map((item) => (
+        <li key={item.id}>
+          <div>
+            <h2>{item.name}</h2>
+            <div className={classes.summary}>
+              <span className={classes.price}>${item.price.toFixed(2)}</span>
+              <span className={classes.amount}>x {item.amount}</span>
+            </div>
+          </div>
+          <div className={classes.actions}>
+            <button onClick={() => cartItemRemoveHandler(item.id)}>−</button>
+            <button onClick={() => cartItemAddHandler(item)}>+</button>
+          </div>
+        </li>
       ))}
     </ul>
   );
@@ -15,13 +42,13 @@ const Cart = (props) => {
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>35.62</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
         <button className={classes['button--alt']} onClick={props.onClose}>
           Close
         </button>
-        <button className={classes.button}>Order</button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );
